ada5a53691f7752223f1c5bc864e591a5ad02c3a
Angeles <angeles.arzalluz@gmail.com>
2021-11-11 12:37:50 +0100

    Added new data and updated documentation

--- R/data.R
+++ R/data.R
@@ -1,14 +1,15 @@
-#' @title Tasic data set (post-downsampling)
+#' @title Tasic et al. 2016 expression matrix
 #'
 #' @description Mouse neural single-cell RNA-Seq data set containing
-#' isoform-level expression obtained using a bulk long-read transcriptome and
-#' single-cell short-reads obtained from Tasic et al. (see reference below).
+#' isoform-level expression. Expression matrix obtained using a bulk long-read
+#' transcriptome and single-cell short-reads obtained from Tasic et al. 2016
+#' (see reference below).
 #'
-#' @format A tibble containing single-cell isoform counts for 241 cells and
-#' 13452 isoforms, represented as columns and rows, respectively.
+#' @format A tibble containing single-cell isoform counts for 1591 cells and
+#' 16240 isoforms, represented as columns and rows, respectively.
 #' Isoform identifiers are defined the \code{transcript_id} column. The data
-#' has been length-normalized, quality filtered and subject to cell type-level
-#' downsampling, as detailed in Arzalluz-Luque et al. (see reference below).
+#' has been length-normalized and quality filtered as detailed in
+#' Arzalluz-Luque et al. (see reference below).
 #'
 #' @references
 #'
@@ -21,32 +22,31 @@
 #' obtained from Tasic et al.:
 #'
 #' \insertRef{Tasic2016a}{acorde}
-"tasic_down"
+"tasic"
 
 
-#' @title Tasic metadata (post-downsampling)
+#' @title Tasic et al. 2016 metadata
 #'
 #' @description Metadata table provided from publicly available experimental
-#' details from Tasic et al. (see references).
+#' details from Tasic et al. 2016 (see references).
 #'
 #' @format A tibble. Contains 10 columns, including cell IDs (taken from
 #' sequencing run IDs in the \code{run} column), cell types and subtypes assgined
 #' by the authors (\code{cell_type} and \code{subtype} columns).
-#'
 #'
 #' @references
 #' Public single-cell RNA-Seq data (Smart-Seq2) from mouse visual cortex was
 #' obtained from Tasic et al.:
 #'
 #' \insertRef{Tasic2016a}{acorde}
-"metadata_down"
+"metadata"
 
 
 #' @title Gene-isoform associations for the mouse neural long read transcriptome
 #'
 #' @description Table associating long read-defined isoform IDs to their genes.
 #'
-#' @format A tibble containing the columns \code{transcript_id} and \code{gene_id}.
+#' @format A tibble containing the columns \code{transcript} and \code{gene}.
 #' Isoform (i.e. transcript) identifiers correspond to those obtained during
 #' long read-transcriptome definition (see Arzalluz Luque et al.)
 #'
@@ -61,17 +61,18 @@
 
 #' @title Isoform Differential Expression analysis results
 #'
-#' @description This object contains isoform Differential Expression results obtained
-#' using edgeR and DESeq2 on the Tasic dataset. Given potential variability across
+#' @description This object contains a list of differentially expressed isoforms
+#' obtained after x50 downsamplping runs combined with Differential Expression
+#' testing using edgeR and DESeq2. Given potential variability across
 #' DE method runs and to ensure the reproducibility of the results in Arzalluz-Luque
 #' et al. (see refs.), we provide the original results used in the manuscript.
 #'
-#' @format A named list of length 2 containing two tibbles including \code{edgeR} and
-#' \code{DESeq2} result tables, respectively. These are output by the
-#' \code{\link{cell_type_DE}} function.
+#' @format A tibble containing a single column, named \code{transcript}.
+#' This transcript list is used to filter the \code{\link{tasic}} expression
+#' matrix to retain only consistently DE transcript isoforms.
 #'
 #' @references
 #'
 #' \insertRef{Arzalluz-Luque2021}{acorde}
-"de_results"
+"consensus_DE_set"
 
